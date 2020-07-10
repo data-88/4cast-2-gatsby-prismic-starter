18b8d536ce50f647456b94313daf1a0959e51a83
AB <23291910+data-88@users.noreply.github.com>
2020-07-10 10:35:14 +0200

    first customisation

--- prismic-config.js
+++ prismic-config.js
@@ -1,4 +1,4 @@
 module.exports = {
   // -- Prismic repository GraphQL endpoint
-  apiEndpoint: 'https://your-repo-name.prismic.io/graphql',
+  apiEndpoint: 'https://4cast.cdn.prismic.io/api/v2',
 };
