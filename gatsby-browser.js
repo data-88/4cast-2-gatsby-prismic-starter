da53871e2d43c0515f3dc4cf9b86eadcc849d9ed
AB <23291910+data-88@users.noreply.github.com>
2020-07-10 15:50:46 +0200

    link resolver

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,9 +2,10 @@
  * Implement Gatsby's Browser APIs in this file.
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
-//  */
+ */
 
-// // You can delete this file if you're not using it
-// const { registerLinkResolver } = require('gatsby-source-prismic-graphql');
+// You can delete this file if you're not using it
 
-// registerLinkResolver(require('./src/prismic/linkResolver').linkResolver);
+const { registerLinkResolver } = require('@prismicio/gatsby-source-prismic-graphql');
+
+registerLinkResolver(require('./src/utils/linkResolver').linkResolver);
